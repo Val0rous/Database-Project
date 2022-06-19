b7c533356c7208ae9d50de1cb1ca39491ac8dcf5
Francesco Valentini <fv26012001@gmail.com>
2022-06-19 12:32:12 +0200

    Update DatabaseViewAdmin.cs

--- DatabaseTestWFA/DatabaseViewAdmin.cs
+++ DatabaseTestWFA/DatabaseViewAdmin.cs
@@ -34,7 +34,7 @@ namespace DatabaseProject
             SBind.DataSource = table;
             TabellaDipendenti.DataSource = SBind;
 
-            dataAdapter = new MySqlDataAdapter(queries.LeggiMagazziniToString(this.IDsede), connection.Connection);
+            dataAdapter = new MySqlDataAdapter(queries.LeggiMagazzini(this.IDsede).CommandText, connection.Connection);
             table = new DataTable();
             dataAdapter.Fill(table);
             SBind = new BindingSource();
@@ -48,12 +48,12 @@ namespace DatabaseProject
             SBind.DataSource = table;
             TabellaTour.DataSource = SBind;
 
-            dataAdapter = new MySqlDataAdapter(queries.LeggiBusinessPartners(this.IDsede).CommandText, connection.Connection);
+            dataAdapter = new MySqlDataAdapter(queries.LeggiBusinessPartner(this.IDsede).CommandText, connection.Connection);
             table = new DataTable();
             dataAdapter.Fill(table);
             SBind = new BindingSource();
             SBind.DataSource = table;
-            TabellaTour.DataSource = SBind;
+            TabellaBusinessPartners.DataSource = SBind;
         }
 
         private void Form1_Load(object sender, EventArgs e)
