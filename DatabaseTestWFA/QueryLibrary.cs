27ece7978e8825fe5c16416366d7cffc126ffd36
RAVAIOLI MICHELE <michele.ravaioli3@studio.unibo.it>
2022-06-20 10:38:18 +0200

    fix periodomedioprenotazioni

--- DatabaseTestWFA/QueryLibrary.cs
+++ DatabaseTestWFA/QueryLibrary.cs
@@ -592,18 +592,18 @@ namespace DatabaseProject
             command.CommandText = "SELECT * FROM percorso WHERE percorso.IDsede="+Convert(IDsede);
             return command;
         }
-        public MySqlCommand LeggiPeriodoMedioPrenotazioneAccessori()
+        public MySqlCommand LeggiPeriodoMedioPrenotazioneAccessori(String PIVA)
         {
             MySqlCommand command = this.Connection.CreateCommand();
-            command.CommandText = "SELECT AVG(CAST(DATEDIFF(DataInizio, DataFine) AS float)) AS PeriodoMedioAccessori "+
-                "FROM Servizi WHERE TipoServizio = 'Noleggio_Accessori' GROUP BY IDSede";
+            command.CommandText = "SELECT S.IDsede, AVG(CAST(DATEDIFF(V.DataInizio, V.DataFine) AS float)) AS PeriodoMedioAccessori "+
+                "FROM Servizi V, Sede S WHERE S.PIVAagenzia = " + Convert(PIVA)+" AND V.TipoServizio = 'Noleggio_Accessori' AND S.IDsede = V.IDsede GROUP BY IDSede";
             return command;
         }
         public MySqlCommand LeggiPeriodoMedioPrenotazioneBici()
         {
             MySqlCommand command = this.Connection.CreateCommand();
-            command.CommandText = "SELECT AVG(CAST(DATEDIFF(DataInizio, DataFine) AS float)) AS PeriodoMedioBici "+
-                "FROM Servizi WHERE TipoServizio = 'Noleggio_Biciclette' GROUP BY IDSede";
+            command.CommandText = "SELECT S.IDsede, AVG(CAST(DATEDIFF(V.DataInizio, V.DataFine) AS float)) AS PeriodoMedioBici "+
+                "FROM Servizi V, Sede S WHERE S.PIVAagenzia = " + Convert(PIVA)+" AND V.TipoServizio = 'Noleggio_Bici' AND S.IDsede = V.IDsede GROUP BY IDSede";
             return command;
         }
         public MySqlCommand LeggiPrenotazioni()
