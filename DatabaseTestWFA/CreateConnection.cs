de71e0719d94ec04ac56bfaad00dbba26f32f6e3
Francesco Valentini <fv26012001@gmail.com>
2022-06-20 11:52:40 +0200

    Update CreateConnection.cs

--- DatabaseTestWFA/CreateConnection.cs
+++ DatabaseTestWFA/CreateConnection.cs
@@ -17,7 +17,7 @@ namespace DatabaseProject
             this.Builder = new MySqlConnectionStringBuilder();
             this.Builder.Server = "localhost";
             this.Builder.UserID = "root";
-            //builder.Password = "SIMONE2001";
+            //this.Builder.Password = "SIMONE2001";
             this.Builder.Password = "S@9WfQHnqrgFPN";
             this.Builder.Database = "gestionale_per_agenzie_rel";
             this.Connection = new MySqlConnection(this.Builder.ToString());
